ecd07d77107b87a3740adbb4e1af423ab10f9d75
emmanuelbenjamin <emmanuelbenjamin1800@gmail.com>
2024-04-05 16:03:04 +0100

    add/about page UI

--- git-tuts/app/page.tsx
+++ git-tuts/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>This is the HomePage UI</h1>
+      <h1>This is the About page User Interface</h1>
     </main>
   );
 }
